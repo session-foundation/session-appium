f17e5c9045aa30c34e871ca3e689934951eea8a2
Audric Ackermann <audric@loki.network>
2024-04-22 13:56:30 +1000

    chore: remove commited config

--- run/test/specs/group_tests.spec.ts
+++ run/test/specs/group_tests.spec.ts
@@ -3,7 +3,7 @@ import { newUser } from "./utils/create_account";
 import { newContact } from "./utils/create_contact";
 import { createGroup } from "./utils/create_group";
 import { runOnlyOnAndroid, runOnlyOnIOS, sleepFor } from "./utils/index";
-import { closeApp, openAppFourDevices, openAppThreeDevices, SupportedPlatformsType } from "./utils/open_app";
+import { SupportedPlatformsType, closeApp, openAppFourDevices, openAppThreeDevices } from "./utils/open_app";
 
 async function groupCreation(platform: SupportedPlatformsType) {
   const testGroupName = "Test group";
@@ -160,37 +160,6 @@ async function addContactToGroup(platform: SupportedPlatformsType) {
   // Check config
   await runOnlyOnIOS(platform, () => device4.waitForControlMessageToBePresent(`${userD.userName} joined the group.`));
   await closeApp(device1, device2, device3, device4);
-}
-
-async function mentionsForGroupsIos(platform: SupportedPlatformsType) {
-  const { device1, device2, device3 } = await openAppThreeDevices(platform);
-  // Create users A, B and C
-  const [userA, userB, userC] = await Promise.all([
-    newUser(device1, "Alice", platform),
-    newUser(device2, "Bob", platform),
-    newUser(device3, "Charlie", platform),
-  ]);
-  const testGroupName = "Mentions test group";
-  // Create contact between User A and User B
-  await createGroup(platform, device1, userA, device2, userB, device3, userC, testGroupName);
-  await device1.inputText("accessibility id", "Message input box", "@");
-  // Check that all users are showing in mentions box
-  await device1.findElement("accessibility id", "Mentions list");
-  // Select User B
-  await device1.selectByText("Contact", userB.userName);
-  await device1.clickOnElement("Send message button");
-  // Check in user B's device if the format is correct
-  await device2.findMessageWithBody("@You");
-  await device2.inputText("accessibility id", "Message input box", "@");
-  // Check that all users are showing in mentions box
-  await device2.findElement("accessibility id", "Mentions list");
-  // Select User C
-  await device2.selectByText("Contact", userC.userName);
-  await device2.clickOnElement("Send message button");
-  // Check in User C's device if the format is correct
-  await device3.findMessageWithBody("@You");
-  // Close app
-  await closeApp(device1, device2, device3);
 }
 
 async function mentionsForGroups(platform: SupportedPlatformsType) {
