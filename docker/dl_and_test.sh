9821e33e8ed125ab02494ff1563b6f769d1fafd8
Audric Ackermann <audric@loki.network>
2024-07-03 17:17:20 +1000

    fix: test gh runner

--- docker/dl_and_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ex
-
-/session-appium/docker/dl.sh && /session-appium/docker/test.sh
--- docker/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ex
-
-cd /session-appium && yarn install --immutable && yarn tsc && yarn test-no-retry ""
