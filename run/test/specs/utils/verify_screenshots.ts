2d67443418e2aba63ef1163259a9c71b114434ee
Miklos Mandoki <miklos@getsession.org>
2025-02-06 11:38:20 +1100

    fix: verifyElementScreenshot enforces states when mandatory

--- run/test/specs/utils/verify_screenshots.ts
+++ run/test/specs/utils/verify_screenshots.ts
@@ -4,15 +4,24 @@ import * as path from 'path';
 import { DeviceWrapper } from '../../../types/DeviceWrapper';
 import { LocatorsInterfaceScreenshot } from '../locators';
 import { v4 as uuidv4 } from 'uuid';
-import { ElementStates } from '../../../types/testing';
 
 // The function takes a screenshot of an element and verifies it against a baseline screenshot
-// If no baseline is available, the element screenshot is retained so that it could be used as a new baseline
+// // Supports locators with optional multiple states, enforcing correct state usage where applicable
+// If no baseline is available, the element screenshot is retained for potential future use as a new baseline
 // The baseline images were taken on a Pixel 6 (1080x2061) and an iPhone 15 Pro Max (1290x2462)
-export async function verifyElementScreenshot(
+//
+// Example usage:
+// Locator with multiple states;
+// await verifyElementScreenshot(device, new EmptyLandingPageScreenshot(device), 'new_account');
+// Locator with a single state:
+// await verifyElementScreenshot(device, new SomeSimpleLocatorScreenshot(device));
+
+export async function verifyElementScreenshot<
+  T extends LocatorsInterfaceScreenshot & { screenshotFileName: (...args: any[]) => string },
+>(
   device: DeviceWrapper,
-  element: LocatorsInterfaceScreenshot,
-  state?: ElementStates
+  element: T,
+  ...args: Parameters<T['screenshotFileName']> // Enforces states when mandatory
 ): Promise<void> {
   // Declaring a UUID in advance so that the diff and screenshot files are matched alphanumerically
   const uuid = uuidv4();
@@ -29,7 +38,7 @@ export async function verifyElementScreenshot(
   const screenshotBuffer = Buffer.from(elementScreenshotBase64, 'base64');
   fs.writeFileSync(elementScreenshotPath, screenshotBuffer);
   // Check if baseline screenshot exists
-  const baselineScreenshotPath = element.screenshotFileName(state);
+  const baselineScreenshotPath = element.screenshotFileName(...args);
   if (!fs.existsSync(baselineScreenshotPath)) {
     throw new Error(
       `No baseline image found at: ${baselineScreenshotPath}. A new screenshot has been saved at: ${elementScreenshotPath}`
