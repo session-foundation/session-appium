e266cca02a64d7e5a2e1925591cd10c519f476f2
wafflesvsfrankie <92288602+burtonemily@users.noreply.github.com>
2025-02-24 14:48:08 +1100

    Need to add in correct simulator for ios

--- run/test/specs/utils/capabilities_ios.ts
+++ run/test/specs/utils/capabilities_ios.ts
@@ -27,7 +27,7 @@ const sharediOSCapabilities: AppiumXCUITestCapabilities = {
   // "appium:isHeadless": true,
 } as AppiumXCUITestCapabilities;
 
-const MAX_CAPABILITIES_INDEX = 8;
+const MAX_CAPABILITIES_INDEX = 12;
 export type CapabilitiesIndexType = IntRange<0, typeof MAX_CAPABILITIES_INDEX>;
 
 export function capabilityIsValid(
@@ -54,6 +54,10 @@ function getIOSSimulatorUUIDFromEnv(index: CapabilitiesIndexType): string {
     'IOS_SIXTH_SIMULATOR',
     'IOS_SEVENTH_SIMULATOR',
     'IOS_EIGHTH_SIMULATOR',
+    'IOS_NINTH_SIMULATOR',
+    'IOS_TENTH_SIMULATOR',
+    'IOS_ELEVENTH_SIMULATOR',
+    'IOS_TWELFTH_SIMULATOR',
   ];
 
   const envVar = envVars[index];
@@ -66,7 +70,7 @@ function getIOSSimulatorUUIDFromEnv(index: CapabilitiesIndexType): string {
   return uuid;
 }
 
-const emulatorUUIDs = Array.from({ length: 8 }, (_, index) =>
+const emulatorUUIDs = Array.from({ length: 12 }, (_, index) =>
   getIOSSimulatorUUIDFromEnv(index as CapabilitiesIndexType)
 );
 
