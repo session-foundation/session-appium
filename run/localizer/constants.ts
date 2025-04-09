720af7c2e731a1cf9beab1cd549b00b48186a014
wafflesvsfrankie <92288602+burtonemily@users.noreply.github.com>
2025-04-09 14:28:00 +1000

    Updates crowdin strings with newest build

--- run/localizer/constants.ts
+++ run/localizer/constants.ts
@@ -2,6 +2,15 @@ export enum LOCALE_DEFAULTS {
   app_name = 'Session',
   session_download_url = 'https://getsession.org/download',
   gif = 'GIF',
+  oxen_foundation = 'Oxen Foundation',
 }
 
-export const rtlLocales = ['ar', 'fa', 'he', 'ps', 'ur'] as const;
+export const rtlLocales = ['ar', 'fa', 'he', 'ps', 'ur'];
+
+export const crowdinLocales = ['en'] as const;
+
+export type CrowdinLocale = (typeof crowdinLocales)[number];
+
+export function isCrowdinLocale(locale: string): locale is CrowdinLocale {
+  return crowdinLocales.includes(locale as CrowdinLocale);
+}
