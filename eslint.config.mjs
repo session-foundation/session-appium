406fefb9f60f4b23fbf9aed3e756ca1c81e270aa
Miklos Mandoki <miklos@getsession.org>
2025-06-25 11:33:17 +1000

    feat: allure report includes visual diffs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default tseslint.config(
       'run/**/*.js',
       'scripts/*.js',
       'avd/',
+      'allure*/',
     ],
   },
   eslint.configs.recommended,
--- run/test/specs/landing_page_restore_account.spec.ts
+++ run/test/specs/landing_page_restore_account.spec.ts
@@ -4,6 +4,7 @@ import { verifyElementScreenshot } from './utils/verify_screenshots';
 import { EmptyLandingPageScreenshot } from './utils/screenshot_paths';
 import { USERNAME } from '@session-foundation/qa-seeder';
 import { linkedDevice } from './utils/link_device';
+import type { TestInfo } from '@playwright/test';
 
 bothPlatformsIt({
   title: 'Landing page restore account',
@@ -12,11 +13,11 @@ bothPlatformsIt({
   countOfDevicesNeeded: 2,
 });
 
-async function landingPageRestoreAccount(platform: SupportedPlatformsType) {
+async function landingPageRestoreAccount(platform: SupportedPlatformsType, testInfo: TestInfo) {
   // Creating a linked device is used as a shortcut to restore an account
   const { device1: alice1, device2: alice2 } = await openAppTwoDevices(platform);
   await linkedDevice(alice1, alice2, USERNAME.ALICE);
   // Verify that the Session logo is shown on the landing page
-  await verifyElementScreenshot(alice2, new EmptyLandingPageScreenshot(alice2), 'restore_account');
+  await verifyElementScreenshot(alice2, new EmptyLandingPageScreenshot(alice2), testInfo, 'restore_account');
   await closeApp(alice1, alice2);
 }
