3c57584d919ae880c72c3e6b78e0968adafa4baa
Miklos Mandoki <miklos@getsession.org>
2025-08-08 17:17:34 +1000

    refactor: no more 4 device tests

--- run/test/specs/linked_group_leave.spec.ts
+++ run/test/specs/linked_group_leave.spec.ts
@@ -2,53 +2,46 @@ import type { TestInfo } from '@playwright/test';
 
 import { englishStrippedStr } from '../../localizer/englishStrippedStr';
 import { bothPlatformsIt } from '../../types/sessionIt';
-import { USERNAME } from '../../types/testing';
 import { ConversationSettings } from './locators/conversation';
 import { LeaveGroupConfirm, LeaveGroupMenuItem } from './locators/groups';
+import { ConversationItem } from './locators/home';
+import { open_Alice2_Bob1_friends_group } from './state_builder';
 import { sleepFor } from './utils';
-import { newUser } from './utils/create_account';
-import { createGroup } from './utils/create_group';
-import { linkedDevice } from './utils/link_device';
-import { closeApp, openAppFourDevices, SupportedPlatformsType } from './utils/open_app';
+import { closeApp, SupportedPlatformsType } from './utils/open_app';
 
 bothPlatformsIt({
   title: 'Leave group linked device',
   risk: 'high',
   testCb: leaveGroupLinkedDevice,
-  countOfDevicesNeeded: 4,
+  countOfDevicesNeeded: 3,
 });
 
 async function leaveGroupLinkedDevice(platform: SupportedPlatformsType, testInfo: TestInfo) {
-  const testGroupName = 'Leave group linked device';
-  const { device1, device2, device3, device4 } = await openAppFourDevices(platform, testInfo);
-  const charlie = await linkedDevice(device3, device4, USERNAME.CHARLIE);
-  // Create users A, B and C
-  const [alice, bob] = await Promise.all([
-    newUser(device1, USERNAME.ALICE),
-    newUser(device2, USERNAME.BOB),
-  ]);
-  // Create group with user A, user B and User C
-  await createGroup(platform, device1, alice, device2, bob, device3, charlie, testGroupName);
+  const testGroupName = 'Linked device group';
+  const {
+    devices: { alice1, alice2, bob1 },
+    prebuilt: { bob },
+  } = await open_Alice2_Bob1_friends_group({
+    platform,
+    groupName: testGroupName,
+    focusGroupConvo: true,
+    testInfo: testInfo,
+  });
+  await bob1.clickOnElementAll(new ConversationSettings(bob1));
   await sleepFor(1000);
-  await device3.clickOnElementAll(new ConversationSettings(device3));
-  await sleepFor(1000);
-  await device3.clickOnElementAll(new LeaveGroupMenuItem(device3));
-  await device3.checkModalStrings(
+  await bob1.clickOnElementAll(new LeaveGroupMenuItem(bob1));
+  await bob1.checkModalStrings(
     englishStrippedStr('groupLeave').toString(),
     englishStrippedStr('groupLeaveDescription').withArgs({ group_name: testGroupName }).toString()
   );
-  // Modal with Leave/Cancel
-  await device3.clickOnElementAll(new LeaveGroupConfirm(device3));
-  // Check for control message
-  await sleepFor(5000);
-  await device4.onIOS().hasTextElementBeenDeleted('Conversation list item', testGroupName);
-  // Create control message for user leaving group
+  await bob1.clickOnElementAll(new LeaveGroupConfirm(bob1));
+  await bob1.verifyElementNotPresent(new ConversationItem(bob1, testGroupName));
   const groupMemberLeft = englishStrippedStr('groupMemberLeft')
-    .withArgs({ name: charlie.userName })
+    .withArgs({ name: bob.userName })
     .toString();
   await Promise.all([
-    device1.waitForControlMessageToBePresent(groupMemberLeft),
-    device2.waitForControlMessageToBePresent(groupMemberLeft),
+    alice1.waitForControlMessageToBePresent(groupMemberLeft),
+    alice2.waitForControlMessageToBePresent(groupMemberLeft),
   ]);
-  await closeApp(device1, device2, device3, device4);
+  await closeApp(alice1, alice2, bob1);
 }
