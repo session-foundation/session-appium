5e870ab6a872ea71dce2aecf2ce6dcfb180bec16
Miklos Mandoki <miklos@getsession.org>
2025-10-06 16:54:54 +1100

    chore: linting

--- run/test/specs/utils/open_app.ts
+++ run/test/specs/utils/open_app.ts
@@ -302,7 +302,7 @@ const openiOSApp = async (
 
   // Apply retry offset, but wrap within the worker's device pool only
   // This means when retrying, alice/bob etc won't be the same device as before within a worker's pool
-  // This is to avoid any issues where a device might be in a bad state for some reason 
+  // This is to avoid any issues where a device might be in a bad state for some reason
   // (e.g. not accessing photo library on iOS)
   const retryOffset = testInfo.retry || 0;
   const deviceIndexWithinWorker = (capabilitiesIndex + retryOffset) % devicesPerWorker;
