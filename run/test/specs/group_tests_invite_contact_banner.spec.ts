d2722d795d2e99d4140f83473c65feabf4a530df
Miklos Mandoki <miklos@getsession.org>
2026-01-05 10:44:35 +1100

    feat: groups banners now check non existence

--- run/test/specs/group_tests_invite_contact_banner.spec.ts
+++ run/test/specs/group_tests_invite_contact_banner.spec.ts
@@ -1,14 +1,13 @@
 import type { TestInfo } from '@playwright/test';
 
-import { androidIt } from '../../types/sessionIt';
+import { bothPlatformsIt } from '../../types/sessionIt';
 import { InviteContactsButton } from './locators';
 import { ConversationSettings } from './locators/conversation';
 import { LatestReleaseBanner, ManageMembersMenuItem } from './locators/groups';
 import { open_Alice1_Bob1_Charlie1_friends_group } from './state_builder';
 import { closeApp, SupportedPlatformsType } from './utils/open_app';
 
-// This banner no longer exists on iOS
-androidIt({
+bothPlatformsIt({
   title: 'Invite contacts banner',
   risk: 'medium',
   testCb: inviteContactGroupBanner,
@@ -36,6 +35,6 @@ async function inviteContactGroupBanner(platform: SupportedPlatformsType, testIn
   await alice1.clickOnElementAll(new ConversationSettings(alice1));
   await alice1.clickOnElementAll(new ManageMembersMenuItem(alice1));
   await alice1.clickOnElementAll(new InviteContactsButton(alice1));
-  await alice1.waitForTextElementToBePresent(new LatestReleaseBanner(alice1));
+  await alice1.verifyElementNotPresent(new LatestReleaseBanner(alice1));
   await closeApp(alice1, bob1, charlie1);
 }
