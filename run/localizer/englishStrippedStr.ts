99ab2c11179881b815a25c42f54dfe18a2ea04b0
Miklos Mandoki <miklos@getsession.org>
2026-02-16 10:15:38 +1100

    refactor: remove dead code

--- run/localizer/englishStrippedStr.ts
+++
@@ -1,6 +0,0 @@
-import { LocalizedStringBuilder, MergedLocalizerTokens } from "./lib";
-
-export function englishStrippedStr<T extends MergedLocalizerTokens>(token: T) {
-  const builder = new LocalizedStringBuilder<T>(token).stripIt().forceEnglish();
-  return builder;
-}
