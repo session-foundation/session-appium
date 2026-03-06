0eab7cdbb2f98d5b4859cd4a15e424c640cf52ff
Miklos Mandoki <miklos@getsession.org>
2026-03-06 11:27:33 +1100

    fix: emulators must have virtual scene configured

--- scripts/setup_virtual_scene.ts
+++ scripts/setup_virtual_scene.ts
@@ -1,6 +1,9 @@
 /**
  * Copies virtual scene config files from the repo to local Android SDK folder if necessary.
  *
+ * NOTE: This only works if the emulators' back camera is set to `virtualscene`:
+ * The config.ini must have a `hw.camera.back=virtualscene` entry. 
+ * 
  * The Toren1BD.posters file keeps track of where to show posters (images) in the virtual camera scene.
  * It has been modified so that the `table` poster shows right in front of where the camera opens, 
  * scaled up 2x, positioned at x: 0, y: 0, z: -1.5. 
